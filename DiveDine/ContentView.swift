462d9eb8833497407a3ea58727535f60a1381ffd
Ravi Ranjan <2796.rravi@gmail.com>
2020-01-30 18:49:56 +0530

    Initial Commit

---
+++ DiveDine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DiveDine
+//
+//  Created by Ravi Singh on 30/01/20.
+//  Copyright © 2020 Ravi Singh. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
